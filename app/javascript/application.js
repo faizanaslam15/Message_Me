c0c983ed0f420166da5ab54759cc0d8a1450f918
Faizan Aslam <faizanaslam@carecloud.com>
2025-05-06 11:25:48 +0500

    semantic-ui added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,23 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+// import "@hotwired/turbo-rails"
+//import "controllers"
+ //= require rails-ujs
+ //= require jquery
+  //= require activestorage
+  //= require turbolinks
+  //= require semantic-ui
+  //= require_tree .
+  //= require init
+
+  // $(document).on('turbolinks:load', function() {
+  //   $('.ui.dropdown').dropdown();
+  //   $('.message .close').on('click', function() {
+  //     $(this).closest('.message').transition('fade');
+  //   });
+  //   submit_message();
+  //   scroll_bottom();
+  // })
+
+  $('.ui.dropdown')
+  .dropdown()
+;
