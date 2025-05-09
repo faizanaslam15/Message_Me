06012dbd00189b9a63ed69e0bb1451a2121f8797
Faizan Aslam <faizanaslam@carecloud.com>
2025-05-09 15:50:12 +0500

    Partials Added for messages

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// import "@hotwired/turbo-rails"
-//import "controllers"
+import "@hotwired/turbo-rails"
+import "controllers"
  //= require rails-ujs
  //= require jquery
   //= require activestorage
@@ -8,16 +8,9 @@
   //= require semantic-ui
   //= require_tree .
   //= require init
-
-  // $(document).on('turbolinks:load', function() {
-  //   $('.ui.dropdown').dropdown();
-  //   $('.message .close').on('click', function() {
-  //     $(this).closest('.message').transition('fade');
-  //   });
-  //   submit_message();
-  //   scroll_bottom();
-  // })
+ 
 
   $('.ui.dropdown')
-  .dropdown()
-;
+  .dropdown();
+
+  
